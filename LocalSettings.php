209e0bc278883e2f0789ebe8f2a95e12cca6f08d
Ron Gross <ron.gross@gmail.com>
2012-12-08 15:45:45 +0200

    Remove DB connection string from the source code!

--- LocalSettings.php
+++ LocalSettings.php
@@ -51,11 +51,17 @@ $wgEnotifWatchlist     = true; # UPO
 $wgEmailAuthentication = true;
 
 ## Database settings
-$wgDBtype           = "postgres";
-$wgDBserver         = "ec2-54-243-223-183.compute-1.amazonaws.com";
-$wgDBname           = "dcc4qtjaqso85s";
-$wgDBuser           = "lzaxekoeywqqii";
-$wgDBpassword       = "WvujzH5kQTuGdrUDd2qzEvD_40";
+$_wgDBConnectionString = getenv('DATABASE_URL');
+if (preg_match('%(.*?)://([^:]+):([^@]+)@([^:]+):(\d+)/(.*)%', $_wgDBConnectionString, $regs, PREG_OFFSET_CAPTURE)) {
+	$wgDBtype = $regs[1][0];
+	$wgDBserver = $regs[2][0];
+	$wgDBuser = $regs[3][0];
+	$wgDBpassword = $regs[4][0];
+	$wgDBname = $regs[5][0];
+} else {
+	die("Failed to parse DB connection string");
+}
+
 
 # Postgres specific settings
 $wgDBport           = "5432";
