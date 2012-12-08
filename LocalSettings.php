5aea1d8ecad495ecf43af98d3f39d16a6d62b70a
Ron Gross <ron.gross@gmail.com>
2012-12-08 14:31:26 +0200

    Move to wiki.bitcoinx.org

--- LocalSettings.php
+++ LocalSettings.php
@@ -29,7 +29,7 @@ $wgScriptPath       = "";
 $wgScriptExtension  = ".php";
 
 ## The protocol and server name to use in fully-qualified URLs
-$wgServer           = "http://mediawiki-heroku.herokuapp.com";
+$wgServer           = "http://wiki.bitcoinx.org";
 
 ## The relative URL path to the skins directory
 $wgStylePath        = "$wgScriptPath/skins";
